fdda864c5fe796a2500f70e7781699c4c91176a0
Dalsania <81883338+DalsaniaCode@users.noreply.github.com>
2025-05-07 15:02:48 -0500

    Update Asnalitean-Table.sql

--- LanguagesSQL/Tables/Asnalitean-Table.sql
+++ LanguagesSQL/Tables/Asnalitean-Table.sql
@@ -2,5 +2,6 @@ CREATE TABLE Asnalitean (
     id INT PRIMARY KEY AUTO_INCREMENT,
     english VARCHAR(30) NOT NULL,
     asnalitean VARCHAR(30) NOT NULL,
-    class VARCHAR(3) NOT NULL
+    class VARCHAR(3) NOT NULL,
+    UNIQUE (english, asnalitean, class)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
