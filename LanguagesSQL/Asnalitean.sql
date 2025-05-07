afa3d27eb7e881b5b7a6824b37c1d70befbb77e0
Dalsania <81883338+DalsaniaCode@users.noreply.github.com>
2025-05-07 14:26:32 -0500

    Update Asnalitean.sql

--- LanguagesSQL/Asnalitean.sql
+++ LanguagesSQL/Asnalitean.sql
@@ -1,5 +1,7 @@
-INSERT INTO Asnalitean (english, asnalitean, class)
+INSERT IGNORE INTO Asnalitean (english, asnalitean, class)
 VALUES
+    ('Because', 'क्लेंवोर', '000'),
     ('Bye', 'शेवू॔', '000'),
     ('Goodbye', 'शेवू॔', '000'),
-    ('Hello', 'हालइवैः', '000');
+    ('Hello', 'हालइवैः', '000'),
+    ('One', 'उगौये', 'NUM');
