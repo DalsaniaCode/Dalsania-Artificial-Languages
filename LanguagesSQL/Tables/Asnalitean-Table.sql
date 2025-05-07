c325992847f0e5bbfbdfc7324ca0810b04581fb8
Dalsania <81883338+DalsaniaCode@users.noreply.github.com>
2025-05-07 12:26:28 -0500

    Update Asnalitean-Table.sql

--- LanguagesSQL/Tables/Asnalitean-Table.sql
+++ LanguagesSQL/Tables/Asnalitean-Table.sql
@@ -3,4 +3,4 @@ CREATE TABLE Asnalitean (
     english VARCHAR(30) NOT NULL,
     asnalitean VARCHAR(30) NOT NULL,
     class VARCHAR(3) NOT NULL
-);
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
