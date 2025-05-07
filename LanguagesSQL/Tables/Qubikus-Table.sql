b42c19ba26b4380c1c649bba1005b95873f846be
Dalsania <81883338+DalsaniaCode@users.noreply.github.com>
2025-05-07 15:03:30 -0500

    Update Qubikus-Table.sql

--- LanguagesSQL/Tables/Qubikus-Table.sql
+++ LanguagesSQL/Tables/Qubikus-Table.sql
@@ -2,5 +2,6 @@ CREATE TABLE Qubikus (
     id INT PRIMARY KEY AUTO_INCREMENT,
     english VARCHAR(30) NOT NULL,
     qubikus VARCHAR(30) NOT NULL,
-    class VARCHAR(3) NOT NULL
+    class VARCHAR(3) NOT NULL,
+    UNIQUE (english, qubikus, class)
 )  CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
